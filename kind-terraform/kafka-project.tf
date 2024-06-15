0314e0ef8337191287f1f3ce40f2bb6c7796b579
Guanqiao Chen <guanqiaochen@Guanqiaos-MacBook-Pro.local>
2024-06-15 16:44:24 +1000

    add argocd koperator project

---
+++ kind-terraform/kafka-project.tf
@@ -0,0 +1,32 @@
+resource "kubectl_manifest" "argocd_kafka_project" {
+  yaml_body = file("${path.module}/../ArgoCD/projects/kafka-project.yaml")
+  depends_on = []
+}
+
+resource "kubectl_manifest" "argocd_cert_manager_application" {
+  count = var.deploy_cert_manager ? 1 : 0
+  yaml_body = file("${path.module}/../ArgoCD/applications/cert-manager-application.yaml")
+  depends_on = [kubectl_manifest.argocd_kafka_project]
+}
+
+resource "kubectl_manifest" "argocd_zookeeper_application" {
+  count = var.deploy_zookeeper ? 1 : 0
+  yaml_body = file("${path.module}/../ArgoCD/applications/zookeeper-application.yaml")
+  depends_on = [
+    kubectl_manifest.argocd_kafka_project,
+    kubectl_manifest.argocd_cert_manager_application
+  ]
+}
+
+resource "kubectl_manifest" "argocd_kafka_operator_application" {
+  count = var.deploy_koperator ? 1 : 0
+  yaml_body = file("${path.module}/../ArgoCD/applications/koperator-application.yaml")
+  depends_on = [
+    kubectl_manifest.argocd_kafka_project,
+    kubectl_manifest.argocd_zookeeper_application,
+    kubectl_manifest.argocd_cert_manager_application,
+  ]
+}
+
+
+
