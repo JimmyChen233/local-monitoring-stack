3e60dca51ab81d2a75c7977968b29ea4e514a9a0
Guanqiao Chen <guanqiaochen@Guanqiaos-MacBook-Pro.local>
2024-06-15 16:55:53 +1000

    add cert-manager

--- kind-terraform/kafka-project.tf
+++ kind-terraform/kafka-project.tf
@@ -1,12 +1,21 @@
+
 resource "kubectl_manifest" "argocd_kafka_project" {
   yaml_body = file("${path.module}/../ArgoCD/projects/kafka-project.yaml")
   depends_on = []
 }
 
+resource "kubectl_manifest" "argocd_namespaces" {
+  yaml_body = file("${path.module}/../ArgoCD/applications/namespace-application.yaml")
+  depends_on = []
+} 
+
 resource "kubectl_manifest" "argocd_cert_manager_application" {
   count = var.deploy_cert_manager ? 1 : 0
   yaml_body = file("${path.module}/../ArgoCD/applications/cert-manager-application.yaml")
-  depends_on = [kubectl_manifest.argocd_kafka_project]
+  depends_on = [
+    kubectl_manifest.argocd_kafka_project,
+    kubectl_manifest.argocd_namespaces
+  ]
 }
 
 resource "kubectl_manifest" "argocd_zookeeper_application" {
@@ -14,6 +23,7 @@ resource "kubectl_manifest" "argocd_zookeeper_application" {
   yaml_body = file("${path.module}/../ArgoCD/applications/zookeeper-application.yaml")
   depends_on = [
     kubectl_manifest.argocd_kafka_project,
+    kubectl_manifest.argocd_namespaces,
     kubectl_manifest.argocd_cert_manager_application
   ]
 }
@@ -23,6 +33,7 @@ resource "kubectl_manifest" "argocd_kafka_operator_application" {
   yaml_body = file("${path.module}/../ArgoCD/applications/koperator-application.yaml")
   depends_on = [
     kubectl_manifest.argocd_kafka_project,
+    kubectl_manifest.argocd_namespaces,
     kubectl_manifest.argocd_zookeeper_application,
     kubectl_manifest.argocd_cert_manager_application,
   ]
