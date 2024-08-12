0404464b32e70e6467450b0b2a69597503b9b3bc
Jimmy Chen <jimmychen@Jimmys-MacBook-Pro.local>
2024-08-12 15:26:14 +1000

    add prom-operator

---
+++ kind-terraform-test/ArgoCD.tf
@@ -0,0 +1,22 @@
+resource "kubectl_manifest" "argocd_namespace" {
+  yaml_body = <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: argocd
+EOF
+}
+
+resource "helm_release" "argo" {
+  name = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd" 
+  namespace  = "argocd" 
+  version    = "7.1.3"
+  depends_on = [kubectl_manifest.argocd_namespace]
+
+# An option for setting values that I generally use
+#   values = [jsonencode({
+#     someKey = "someValue"
+#   })]
+}
