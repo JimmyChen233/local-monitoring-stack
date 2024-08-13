74bbd91055974241666ac131cd75fcd7463b9ab1
Jimmy Chen <jimmychen@Jimmys-MacBook-Pro.local>
2024-08-13 13:59:15 +1000

    add grafana-operator

--- kind-terraform/default-project.tf
+++ kind-terraform/default-project.tf
@@ -14,9 +14,19 @@ resource "kubectl_manifest" "argocd_crd" {
   depends_on = [helm_release.argo]
 } 
 
-resource "kubectl_manifest" "argocd_prometheus_application" {
+resource "kubectl_manifest" "argocd_prometheus_operator_application" {
   count = var.deploy_cert_manager ? 1 : 0
-  yaml_body = file("${path.module}/../ArgoCD/applications/prometheus-application.yaml")
+  yaml_body = file("${path.module}/../ArgoCD/applications/prometheus-operator-application.yaml")
+  depends_on = [
+    kubectl_manifest.argocd_default_project,
+    kubectl_manifest.argocd_namespaces,
+    kubectl_manifest.argocd_crd
+  ]
+}
+
+resource "kubectl_manifest" "argocd_grafana_operator_application" {
+  count = var.deploy_cert_manager ? 1 : 0
+  yaml_body = file("${path.module}/../ArgoCD/applications/grafana-operator-application.yaml")
   depends_on = [
     kubectl_manifest.argocd_default_project,
     kubectl_manifest.argocd_namespaces,
