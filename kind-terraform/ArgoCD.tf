a1e4706e92676e9e79b94263a159af81f77dcd64
Jimmy Chen <jimmychen@Jimmys-MacBook-Pro.local>
2024-08-13 15:04:33 +1000

    add grafana-operator

--- kind-terraform/ArgoCD.tf
+++ kind-terraform/ArgoCD.tf
@@ -7,13 +7,34 @@ metadata:
 EOF
 }
 
+resource "kubectl_manifest" "oci_secret" {
+  yaml_body = <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: docker-io-helm-oci
+  namespace: argocd
+  labels:
+    argocd.argoproj.io/secret-type: repository
+stringData:
+  url: oci://ghcr.io/grafana/helm-charts
+  name: grafana-operator
+  type: helm
+  enableOCI: "true"
+EOF
+
+  depends_on = [
+    kubectl_manifest.argocd_namespace
+  ]
+}
+
 resource "helm_release" "argo" {
   name = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd" 
   namespace  = "argocd" 
   version    = "7.1.3"
-  depends_on = [kubectl_manifest.argocd_namespace]
+  depends_on = [kubectl_manifest.argocd_namespace,kubectl_manifest.oci_secret]
 
 # An option for setting values that I generally use
 #   values = [jsonencode({
