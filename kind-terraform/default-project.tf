fa4b27039e92b851add8d87ec6512bcce3be9d09
Guanqiao Chen <guanqiaochen@Guanqiaos-MacBook-Pro.local>
2024-08-18 12:12:07 +1000

    add blackbox and cloudwatch exporters

--- kind-terraform/default-project.tf
+++ kind-terraform/default-project.tf
@@ -36,7 +36,7 @@ resource "kubectl_manifest" "argocd_grafana_operator_application" {
 
 resource "kubectl_manifest" "argocd_blackbox_exporter_application" {
   count = var.deploy_blackbox_exporter ? 1 : 0
-  yaml_body = file("${path.module}/../ArgoCD/applications/blackbox_exporter-application.yaml")
+  yaml_body = file("${path.module}/../ArgoCD/applications/blackbox-exporter-application.yaml")
   depends_on = [
     kubectl_manifest.argocd_default_project,
     kubectl_manifest.argocd_namespaces,
@@ -46,7 +46,7 @@ resource "kubectl_manifest" "argocd_blackbox_exporter_application" {
 
 resource "kubectl_manifest" "argocd_cloudwatch_exporter_application" {
   count = var.deploy_cloudwatch_exporter ? 1 : 0
-  yaml_body = file("${path.module}/../ArgoCD/applications/cloudwatch_exporter-application.yaml")
+  yaml_body = file("${path.module}/../ArgoCD/applications/cloudwatch-exporter-application.yaml")
   depends_on = [
     kubectl_manifest.argocd_default_project,
     kubectl_manifest.argocd_namespaces,
