baec6900c2617771934cc9b1ae8a8fd92940c721
Jimmy Chen <jimmychen@Jimmys-MacBook-Pro.local>
2024-09-20 17:19:47 +1000

    add external dns

--- Terraform/argo-project/default-project.tf
+++ Terraform/argo-project/default-project.tf
@@ -38,9 +38,7 @@ resource "kubectl_manifest" "argocd_prometheus_operator_application" {
   count     = var.deploy_prometheus_operator ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/prometheus-operator-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd
+    kubectl_manifest.argocd_traefik_application
   ]
 }
 
@@ -48,9 +46,6 @@ resource "kubectl_manifest" "argocd_grafana_operator_application" {
   count     = var.deploy_grafana_operator ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/grafana-operator-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd,
     kubectl_manifest.argocd_prometheus_operator_application
   ]
 }
@@ -59,9 +54,6 @@ resource "kubectl_manifest" "argocd_prometheus_config_application" {
   count     = var.deploy_prometheus_operator ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/prometheus-config-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd,
     kubectl_manifest.argocd_prometheus_operator_application
   ]
 }
@@ -70,9 +62,7 @@ resource "kubectl_manifest" "argocd_blackbox_exporter_application" {
   count     = var.deploy_blackbox_exporter ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/blackbox-exporter-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd
+    kubectl_manifest.argocd_prometheus_operator_application
   ]
 }
 
@@ -80,9 +70,7 @@ resource "kubectl_manifest" "argocd_cloudwatch_exporter_application" {
   count     = var.deploy_cloudwatch_exporter ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/cloudwatch-exporter-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd
+    kubectl_manifest.argocd_prometheus_operator_application
   ]
 }
 
@@ -90,9 +78,6 @@ resource "kubectl_manifest" "argocd_sloth_application" {
   count     = var.deploy_sloth ? 1 : 0
   yaml_body = file("${path.module}/../../ArgoCD/applications/sloth-application.yaml")
   depends_on = [
-    kubectl_manifest.argocd_default_project,
-    kubectl_manifest.argocd_namespaces,
-    kubectl_manifest.argocd_crd,
     kubectl_manifest.argocd_prometheus_operator_application
   ]
 }
